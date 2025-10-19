0c92c72820b79eefc3f6171e59ef69bdced62c4f
Annguyen0410 <Nguyenan04102004@gmail.com>
2025-10-19 13:34:19 -0700

    Mid Term Project Code - Movie Explorer - An Nguyen Commit

--- app/src/main/java/com/example/cpsc411mid_termproject/MainActivity.kt
+++ app/src/main/java/com/example/cpsc411mid_termproject/MainActivity.kt
@@ -5,43 +5,28 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.cpsc411mid_termproject.navigation.MovieNavigation
 import com.example.cpsc411mid_termproject.ui.theme.CPSC411MidTermProjectTheme
+import com.example.cpsc411mid_termproject.viewmodel.MovieViewModel
 
+// TODO: add splash screen maybe?
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             CPSC411MidTermProjectTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MovieNavigation()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CPSC411MidTermProjectTheme {
-        Greeting("Android")
     }
 }
